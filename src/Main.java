07d741859bf5a2ff6763eb7dc130dfac3b4f295e
shaft163rus <shaft163rus@gmail.com>
2023-04-26 08:35:09 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+       task1();
+//        task2();
+//        task3();
+//        task4();
+//        task5();
+//        task6();
+//        task7();
+//        task8();
+//        task9();
+//        task10();
+    }
+
+
+    public static void task1() {
+        System.out.println("Task1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
